3e742ee78824e38f27230e71b6c3e672b6c2cf8e
Raashmishree-N <raashmishree2005@gmail.com>
2025-05-27 23:14:53 +0530

    Add files via upload

---
+++ MODULE 3 CORE JAVA/Calci.java
@@ -0,0 +1,8 @@
+
+public class Calci{
+    public static void main(String[] args)
+    Scanner sc=new Scanner(System.in);
+    int a=sc.newInt();
+    int b=sc.newInt();
+    
+}
---
+++ MODULE 3 CORE JAVA/HelloWorld.java
@@ -0,0 +1,4 @@
+public class HelloWorld{
+    public static void main(Strings[]args)
+    System.out.print("Hello,World!");
+}
